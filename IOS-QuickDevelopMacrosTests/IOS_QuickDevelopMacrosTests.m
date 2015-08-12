f3a5dba10981fa705c0a278a041ed990420cd342
vilyever <250450035@163.com>
2015-08-12 09:49:28 +0800

    Initial Commit

---
+++ IOS-QuickDevelopMacrosTests/IOS_QuickDevelopMacrosTests.m
@@ -0,0 +1,40 @@
+//
+//  IOS_QuickDevelopMacrosTests.m
+//  IOS-QuickDevelopMacrosTests
+//
+//  Created by FTET on 15/8/12.
+//  Copyright (c) 2015年 vilyever. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface IOS_QuickDevelopMacrosTests : XCTestCase
+
+@end
+
+@implementation IOS_QuickDevelopMacrosTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
